48246f5b10d48ccb05ddc2b1c431bb275cfb3aa1
YeffryF <yeffry.fermin.ttp@gmail.com>
2026-07-29 21:57:31 -0400

    fix organization of components and added UserHeader component

---
+++ src/components/UserHeader/UserHeader.jsx
@@ -0,0 +1,24 @@
+// UserHeader.jsx
+import React from 'react';
+import './UserHeader.css'
+
+
+const UserHeader = ({ name, role, timeAgo }) => {
+  return (
+    <div className="user-header-container">
+        {/* For now make a rounded gray circle to simulate */}
+      <div className='avatar-img'></div> 
+      
+      <div className="">
+        <h4 className="user-name">
+          {name}
+        </h4>
+        <div className="timestamp">
+          <span>Posted {timeAgo}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserHeader;
